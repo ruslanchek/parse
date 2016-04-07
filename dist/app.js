68bd243fa5d0ae7ff10cac5f6197ce697f73eb71
Ruslan Shashkov <rshashkov@icloud.com>
2016-04-07 10:04:25 +0300

    Init

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var x = 123;
+//# sourceMappingURL=app.js.map
